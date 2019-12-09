1b7609de0ad6ea8631d35d6ab88bdcc2a809dccc
Sephy42 <mlop-65@hotmail.fr>
2019-12-09 16:47:12 +0100

    test de commit

--- src/main/java/com/formation/persistence/entities/Client.java
+++ src/main/java/com/formation/persistence/entities/Client.java
@@ -9,7 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+//testeuh
 @ Table (name = "clients") @Entity
 public class Client {
 	@Id
