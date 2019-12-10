e76837e915ba23f893ede0a308ca8d0dd10b498f
Sephy42 <mlop-65@hotmail.fr>
2019-12-10 09:44:35 +0100

    un nouveau test

--- src/main/java/com/formation/persistence/entities/Client.java
+++ src/main/java/com/formation/persistence/entities/Client.java
@@ -9,7 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-// je fais un test
+// je fais un autre testeiiiih
 @ Table (name = "clients") @Entity
 public class Client {
 	@Id
