81daaf43bb9905529cf8ac39511443b3aa17d7ab
adelie.thirion@gmail.com <adelie.thirion@gmail.com>
2019-12-10 15:36:38 +0100

    DTO Place

---
+++ src/main/java/com/formation/dto/place/PlaceFull.java
@@ -0,0 +1,35 @@
+package com.formation.dto.place;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table (name = "places") @Entity
+public class PlaceFull {
+	
+	Long id;
+	String label;
+	String address;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+}
