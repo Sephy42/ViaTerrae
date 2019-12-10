5c33274c7bb7f7514f4f255e5cdc928ff2d40cd7
emmy.ventou@orange.fr <emmy.ventou@orange.fr>
2019-12-10 11:24:14 +0100

    nouvelle version

--- src/main/java/com/formation/controllers/AuthController.java
+++ src/main/java/com/formation/controllers/AuthController.java
@@ -1,10 +1,11 @@
 package com.formation.controllers;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/auth/")
 public class AuthController {
 	
-	
+
 
 }
