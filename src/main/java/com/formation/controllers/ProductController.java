51c0a91efec57f304796a343b031a62a91c4149a
emmy.ventou@orange.fr <emmy.ventou@orange.fr>
2019-12-10 15:18:49 +0100

    new controllers

---
+++ src/main/java/com/formation/controllers/ProductController.java
@@ -0,0 +1,11 @@
+package com.formation.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/private/product")
+public class ProductController {
+
+	
+}
