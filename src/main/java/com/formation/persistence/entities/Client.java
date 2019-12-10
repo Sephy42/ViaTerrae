412c81262703c745ad8cea487f3632198f04baa7
Sephy42 <mlop-65@hotmail.fr>
2019-12-10 09:59:01 +0100

    Base saine, fin des tests git

--- src/main/java/com/formation/persistence/entities/Client.java
+++ src/main/java/com/formation/persistence/entities/Client.java
@@ -10,10 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-// je fais un autre testeiiiih a
-// Margot a
-// Je rajoute un test B
-// Je rajoute mon nom Baptiste B
+
 @ Table (name = "clients") @Entity
 public class Client {
 	@Id
