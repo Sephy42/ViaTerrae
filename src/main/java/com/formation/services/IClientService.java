5b48bfbcdd7bf9a033e7366528211163490a9de2
Sephy42 <mlop-65@hotmail.fr>
2019-12-10 17:20:02 +0100

    ça marche

--- src/main/java/com/formation/services/IClientService.java
+++ src/main/java/com/formation/services/IClientService.java
@@ -1,20 +1,12 @@
 package com.formation.services;
 
-<<<<<<< HEAD
-import org.springframework.stereotype.Service;
-=======
 import org.springframework.transaction.annotation.Transactional;
->>>>>>> branch 'devel' of https://github.com/Sephy42/ViaTerrae.git
 
 import com.formation.persistence.entities.Client;
 import com.formation.services.common.IServiceActions;
 
-<<<<<<< HEAD
 
-
-=======
 @Transactional
->>>>>>> branch 'devel' of https://github.com/Sephy42/ViaTerrae.git
 public interface IClientService extends IServiceActions<Client> {
 
 	Client findByMail(String username);
