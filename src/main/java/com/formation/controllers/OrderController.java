1b33297cc3d81726c002c70f43e28fd118ecc56f
Avrenak <baptiste.aurelien@gmail.com>
2019-12-11 12:36:35 +0100

    Modification des commandes findAll et findOne

--- src/main/java/com/formation/controllers/OrderController.java
+++ src/main/java/com/formation/controllers/OrderController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.formation.dto.order.OrderFull;
 import com.formation.exceptions.NotAuthorizedException;
+import com.formation.persistence.entities.Admin;
 import com.formation.persistence.entities.Client;
 import com.formation.persistence.entities.Order;
 import com.formation.services.IAuthChecker;
@@ -30,28 +31,61 @@ public class OrderController {
 	@Autowired
 	private IOrderService servOrder;
 	
+	
+	/**
+	 * @param id client
+	 * @param Authentification (isClient or isAdmin or notFound)
+	 * @return all orders from client selected by his id
+	 * @throws Not Authorized if auth failed
+	 * @throws Not found if client is not found
+	 */
 	@GetMapping (path = "/{id}")
 	public OrderFull findOne (@PathVariable Long id) {
 		
 		Client me = authChecker.getCurrentClient();
 		
 		Order order = servOrder.findOne(id);
-		if (order.getClient().equals(me))
+		if (order.getClient().equals(me)) 
+		{
 			return mapper.map(order, OrderFull.class);
-		
-		throw new NotAuthorizedException("C'est pas toi !");
-		
+		} else {
+			if (authChecker.getCurrentAdmin() != null) {
+				return mapper.map(servOrder.findOne(id), OrderFull.class);
+			} else {
+				throw new NotAuthorizedException("Non autorisé !");
+			}
+		}
+
 	}
 	
+	
+	
+	/**
+	 * @param Authentification (isClient ou isAdmin ou notFound)
+	 * @return all the commands found in database accessible for token
+	 * @throws Not Authorized if auth failed
+	 * @throws Not found if client is not found
+	 */
 	@GetMapping (path = "/list")
 	public List<OrderFull> findAll() {
+		
 		Client me = authChecker.getCurrentClient();
-		
-		System.out.println("Called by " + me.getName());
-		
-		return servOrder.findAllById(me.getId())
-				.stream()
-				.map(u -> mapper.map(u, OrderFull.class))
-				.collect(Collectors.toList());
+		if (me != null) 
+		{
+			return servOrder.findAllById(me.getId())
+					.stream()
+					.map(u -> mapper.map(u, OrderFull.class))
+					.collect(Collectors.toList());
+		} else {
+			if (authChecker.getCurrentAdmin() != null) {
+				return servOrder.findAll()
+						.stream()
+						.map(u -> mapper.map(u, OrderFull.class))
+						.collect(Collectors.toList());
+			} else {
+				throw new NotAuthorizedException("Non autorisé !");
+			}
+		}
+
 	}
 }
