6c40164037b16e62e5472bfeb54472e49f5ac627
Avrenak <baptiste.aurelien@gmail.com>
2020-01-15 12:30:33 +0100

    Ajout du Cross Origin pour tester avec Angular

--- src/main/java/com/formation/controllers/PublicController.java
+++ src/main/java/com/formation/controllers/PublicController.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,7 @@ import com.formation.services.IPlaceService;
  */
 @RestController
 @RequestMapping(path = "/api/public")
+@CrossOrigin
 public class PublicController {
 
 	
